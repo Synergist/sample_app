0ef6883808bc5e6e8f59077b2ca7f73561024d5c
Pranav Singh <pranavsingh92@gmail.com>
2012-05-25 03:27:28 -0400

    Added logo helper method to application_helper.rb and test for right links on the layout to layout_links_spec.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  # Helper method for logo. See _header.html.erb for use.
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
