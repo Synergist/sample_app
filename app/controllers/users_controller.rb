f3a9ade634c11a7ab353b86bfc426db37506c4c3
Pranav Singh <pranavsingh92@gmail.com>
2012-06-09 03:56:18 -0400

    Done with user edit/update, index, and detroy actions. (RoR Tutorial completed through Ch10)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_filter :admin_user,    :only => :destroy
+  before_filter :authenticate,  :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user,  :only => [:edit, :update]
+  before_filter :not_signed_in, :only => [:new, :create]
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+  
   def new
     @user = User.new
     @title = "Sign up"
@@ -21,4 +31,49 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render :edit
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    if current_user?(user)
+      flash[:notice] = "Admin users may not delete themselves."
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+    end
+    redirect_to users_path
+  end
+  
+  private
+    
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless (signed_in? && 
+                                    current_user.admin?)
+    end
+    
+    def not_signed_in
+      redirect_to(root_path) if signed_in?
+    end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,11 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
   user.name                   "John Doe"
-  f.sequence(:email) { |n| "foo#{n}@example.com" }
+  user.email                  "jdoe@example.com"
   user.password               "foobar"
   user.password_confirmation  "foobar"
+end
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
 end
