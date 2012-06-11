6733a67f15e5b23b1b16bffbac75ec9c43187b11
Pranav Singh <pranavsingh92@gmail.com>
2012-06-11 13:07:42 -0400

    User relationships (following/followers)

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,16 @@ require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :email, :name, :password, :password_confirmation
+  
   has_many :microposts, :dependent => :destroy
+  has_many :relationships,  :dependent => :destroy,
+                            :foreign_key => "follower_id"
+  has_many :reverse_relationships,  :dependent => :destroy, 
+                                    :foreign_key => "followed_id",
+                                    :class_name => "Relationship" 
+  has_many :following,  :through => :relationships, :source => :followed 
+  has_many :followers,  :through => :reverse_relationships,
+                        :source => :follower
   
   # Regular expression to match emails with a valid format. 
     # \A          : match start of a string
@@ -40,29 +49,42 @@ class User < ActiveRecord::Base
     Micropost.where(:user_id => id)
   end
   
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
+  
   # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
 
-  # ------------------------------------------------------------
-  # 'authenticate' is a class method. We could've equivalently used: 
-  # 1.  def User.authenticate(email, submitted_password)...end, or
-  # 2.  class << self
-  #       def authenticate(email, submitted_password)...end
-  #     end
-  # ------------------------------------------------------------
-  # The 'authenticate' method using the ternary operator:
-  def self.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    (user && user.has_password?(submitted_password)) ? user : nil
-  end
+  class << self
+    # ------------------------------------------------------------
+    # 'authenticate' is a class method. We could've equivalently used: 
+    # 1.  def User.authenticate(email, submitted_password)...end, or
+    # 2.  class << self
+    #       def authenticate(email, submitted_password)...end
+    #     end
+    # ------------------------------------------------------------
+    # The 'authenticate' method using the ternary operator:
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
   
-  def self.authenticate_with_salt(id, cookie_salt)
-    user = find_by_id(id)
-    (user && user.salt == cookie_salt) ? user : nil 
-  end
-  
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil 
+    end
+  end 
   
   private
     def encrypt_password
